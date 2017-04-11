7a0754ad22f3b1127e2137a4a8a76619799e8b5d
Todd Sutherland <sutho100@gmail.com>
2017-04-11 22:06:18 +1000

    javascript form validate updated

--- js/main.js
+++ js/main.js
@@ -1,39 +1,41 @@
+//validate
+function validateMyForm()
+{
+    var validationStatus = true;
+    $("#result").text("");
+
+    var name = $('#name').val();
+
+    //validate name
+    if (name.length == 0) {
+        $('#resultName').text("Please enter your name"); // This Segment Displays The Validation Rule For All Fields
+        $("#resultName").css("color", "red");
+        $("#name").focus();
+        validationStatus = false;
+    }
+
+    //validate email
+    var emailText = $('#email').val();
+    if (validateEmail(emailText)) {
+        $("#result").text(emailText + " is valid :)");
+        $("#result").css("color", "green");
+    } else {
+        $("#result").text(emailText + " is not valid :(");
+        $("#result").css("color", "red");
+        //$("#result").focus();
+        validationStatus = false;
+    }
+
+    //check if form failed
+    if (validationStatus) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 
-// autoComplete search
-
-  $( function() {
-    var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
-    $( ".form-control" ).autocomplete({
-      source: availableTags
-    });
-  } );
-
-//autocomplete hack width
-$.ui.autocomplete.prototype._resizeMenu = function () {
-  var ul = this.menu.element;
-  ul.outerWidth(this.element.outerWidth());
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 }
---
+++ js/search.js
@@ -0,0 +1,37 @@
+﻿// autoComplete search
+
+$(function () {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $(".form-control").autocomplete({
+        source: availableTags
+    });
+});
+
+//autocomplete hack width
+$.ui.autocomplete.prototype._resizeMenu = function () {
+    var ul = this.menu.element;
+    ul.outerWidth(this.element.outerWidth());
+}
